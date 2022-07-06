2f3246c98c8b4be6f71e4b6585f1631028ad0865
rf-developer <reedyfeggins@google.com>
2022-07-06 20:13:32 +0000

    new files

--- ex1-simple-vm/main.tf
+++ ex1-simple-vm/main.tf
@@ -1,37 +1,38 @@
 #create a VM using Terraform
 
 provider "google" {
-  project = "ENTER YOUR PROJECT ID HERE"
+  project = "plated-hearth-351522"
   region = "europe-west1"
   zone = "europe-west1-b"
 }
 
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "create_vm" {
   name = "gce-europe-west1-b-d-tflab01"
   machine_type = "n1-standard-4"
   zone = "europe-west1-b"
 
+  allow_stopping_for_update = true
+
   tags = ["tflab01"]
 
+  shielded_instance_config{
+      enable_secure_boot          = true
+      enable_vtpm                 = true
+      enable_integrity_monitoring = true
+    }
+  
   boot_disk {
-    device_name = "gce-europe-west1-b-d-tflab01"
     initialize_params {
-      size = "20"
-      type = "pd-standard"
-      image = "rhel-cloud/rhel-7"
+      image = "debian-cloud/debian-11-bullseye-v20220621"
     }
   }
 
-  scheduling {
-    on_host_maintenance = "MIGRATE"
-  }
-
   network_interface {
-    network = "default"
+     network = "terraform-network"
   }
 
   metadata = {
-    lab = "tflab01"
+    lab = "tflab01"	
   }
 
   metadata_startup_script = "echo Hello World!"
